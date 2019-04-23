885f2907961dee3a8aff735d21b0f9e031b4edf6
steven mcgrath <mcgrath.steven7@gmail.com>
2019-04-22 21:54:55 -0400

    added some themed styles. Basic styles applied to Page and Header

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
+
+const theme = {
+  red: '#FF1234',
+  black: '#303030',
+  grey: '#3B3B3B',
+  lightGrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0,0,0,0.09)',
+  largeBreakpoint: '1300px',
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 class Page extends Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
