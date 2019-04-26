ded3e3e7b21f28317f10cb174273e52041b790c0
steven mcgrath <mcgrath.steven7@gmail.com>
2019-04-26 18:59:51 -0400

    set up basic resolvers for item query and mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,9 @@
-const Mutation = {};
+const Mutation = {
+  async createItem(parent, args, context, info) {
+    // TODO verify user is logged in
+    const item = await context.db.mutation.createItem({ data: { ...args } }, info);
+    return item;
+  },
+};
 
 module.exports = Mutation;
