355cb901e3358c3056dbc2979f48773640144ad2
steven mcgrath <mcgrath.steven7@gmail.com>
2019-04-28 08:45:45 -0400

    completed item sell form, and set up image upload with cloudinary

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,8 +1,7 @@
+const { forwardTo } = require('prisma-binding');
 const Query = {
-  async items(parent, args, context, info) {
-    const items = await context.db.query.items();
-    return items;
-  },
+  items: forwardTo('db'),
+  item: forwardTo('db'),
 };
 
 module.exports = Query;
