da26b8773df1cf0525d6a3d0993397413a2e3e9e
steven mcgrath <mcgrath.steven7@gmail.com>
2019-04-28 09:51:17 -0400

    started building items view as well as individual item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,8 +1,21 @@
 const Mutation = {
-  async createItem(parent, args, context, info) {
+  async createItem(parent, args, ctx, info) {
     // TODO verify user is logged in
-    const item = await context.db.mutation.createItem({ data: { ...args } }, info);
+    const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+    delete updates.id;
+    return ctx.db.mutation.updateItem(
+      {
+        data: { updates },
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
   },
 };
 
